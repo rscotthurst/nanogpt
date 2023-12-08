a2dbfc0cfbe4ee5e1ac73e26cc6ad0ff3ee7f934
Scott Hurst <scott@rscotthurst.com>
2023-12-08 21:38:03 +0000

    Add blocks to intersperse communication and computation

--- bigram.py
+++ bigram.py
@@ -120,7 +120,26 @@ class FeedForward(nn.Module):
         return self.net(x)
 
 
-# super simple bigram model
+# Intersperse the communication and computation with a block
+class Block(nn.Module):
+    """Transformer block: communication followed by computation"""
+
+    def __init__(self, n_embd, n_head):
+        # n_embd: embedding dimension
+        # n_head: the number of heads we'd like
+        super().__init__()
+        head_size = n_embd // n_head
+        self.sa = MultiHeadAttention(n_head, head_size)
+        self.ffwd = FeedForward(n_embd)
+
+    def forward(self, x):
+        x = self.sa(x)  # communication
+        x = self.ffwd(x)  # computation
+
+        return x
+
+
+# super simple bigram model will be built on piece by piece
 class BigramLanguageModel(nn.Module):
 
     def __init__(self, vocab_size):
@@ -128,8 +147,14 @@ class BigramLanguageModel(nn.Module):
         # each token directly reads off the logits for the next token from a lookup table
         self.token_embedding_table = nn.Embedding(vocab_size, n_embd)
         self.position_embedding_table = nn.Embedding(block_size, n_embd)
-        self.sa_heads = MultiHeadAttention(4, n_embd // 4)  # ie 4 heads of 8-dimensional self-attention
-        self.ffwd = FeedForward(n_embd)
+
+        # Intersperse communication and feed forward many times
+        self.blocks = nn.Sequential(
+            Block(n_embd, n_head=4),
+            Block(n_embd, n_head=4),
+            Block(n_embd, n_head=4),
+        )
+
         self.lm_head = nn.Linear(n_embd, vocab_size)
 
     def forward(self, idx, targets=None):
@@ -139,8 +164,7 @@ class BigramLanguageModel(nn.Module):
         tok_emb = self.token_embedding_table(idx)  # token embedding (B,T,C)
         pos_emb = self.position_embedding_table(torch.arange(T, device=device))  # positional embedding (T, C)
         x = tok_emb + pos_emb  # Now x holds both token identifies AND positions at which they occur  (B,T,C)
-        x = self.sa_heads(x)  # Apply one head of self-attention. (B,T,C)
-        x = self.ffwd(x)  # (B,T,C)
+        x = self.blocks(x)  # (B,T,C)
         logits = self.lm_head(x)  # (B,T,vocab_size)
 
         if targets is None:
