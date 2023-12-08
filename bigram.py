19f8a9125fbd74644e724a22f8a3b2284f480cd6
Scott Hurst <scott@rscotthurst.com>
2023-12-08 21:46:48 +0000

    Block optimization 1: add residual pathway

--- bigram.py
+++ bigram.py
@@ -99,9 +99,12 @@ class MultiHeadAttention(nn.Module):
     def __init__(self, num_heads, head_size):
         super().__init__()
         self.heads = nn.ModuleList([Head(head_size) for _ in range(num_heads)])
+        self.proj = nn.Linear(n_embd, n_embd)
 
     def forward(self, x):
-        return torch.cat([h(x) for h in self.heads], dim=-1)
+        out = torch.cat([h(x) for h in self.heads], dim=-1)
+        out = self.proj(out)
+        return out
 
 
 # FeedForward is a simple multi-layer perceptron
@@ -112,8 +115,9 @@ class FeedForward(nn.Module):
     def __init__(self, n_embd):
         super().__init__()
         self.net = nn.Sequential(
-            nn.Linear(n_embd, n_embd),
+            nn.Linear(n_embd, 4 * n_embd),
             nn.ReLU(),
+            nn.Linear(4 * n_embd, n_embd),
         )
 
     def forward(self, x):
@@ -133,8 +137,8 @@ class Block(nn.Module):
         self.ffwd = FeedForward(n_embd)
 
     def forward(self, x):
-        x = self.sa(x)  # communication
-        x = self.ffwd(x)  # computation
+        x = x + self.sa(x)  # communication
+        x = x + self.ffwd(x)  # computation
 
         return x
 
